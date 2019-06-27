ccc70bd58b402e340c429fa2186a8a6616c19409
hulane <huulane@gmail.com>
2019-06-27 09:57:20 +0800

    add options page

---
+++ js/options.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import browser from './utils/browser.js'
+import { log, dir } from './utils/utils.js'
+/* eslint-enable */
+
+const __main = () => {
+  log('browser', browser)
+}
+
+__main()
