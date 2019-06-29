5d8064f7822aeb66109c74734262623e6852821d
hulane <huulane@gmail.com>
2019-06-29 14:03:02 +0800

    using webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const entry = {}
+const filenames = ['background', 'init', 'main', 'options']
+filenames.forEach(filename => {
+  entry[filename] = `./src/js/${filename}.js`
+})
+
+module.exports = {
+  mode: 'production',
+  entry,
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  // optimization: { runtimeChunk: true },
+  plugins: [
+    new BundleAnalyzerPlugin({ analyzerPort: 8081 }),
+    new CleanWebpackPlugin()
+  ],
+  resolve: {
+    alias: {
+    },
+    extensions: ['*', '.js', '.json']
+  },
+  performance: {
+    hints: 'warning'
+  }
+}
